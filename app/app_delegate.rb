5cbc8687533e9a6d050007d96ea40fe69623f1a8
Ryan Sonnek <ryan@codecrate.com>
2014-10-20 18:26:24 +0200

    test simple views

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,8 +1,23 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    alert = UIAlertView.new
-    alert.message = "Hello World!"
-    alert.show
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.makeKeyAndVisible
+
+    label = UILabel.new
+    label.text = 'Hello RubyMotion'
+    label.frame = [[20, 100], [280, 30]]
+    @window.addSubview(label)
+
+    red_square = UIView.new
+    red_square.frame = [[135, 150], [50, 50]]
+    red_square.backgroundColor = UIColor.redColor
+    @window.addSubview(red_square)
+
+    @window.backgroundColor = UIColor.grayColor
+
+    # alert = UIAlertView.new
+    # alert.message = "Hello World!"
+    # alert.show
     true
   end
 end
