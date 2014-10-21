9fea9ab348759bd2ddae4a23f6928f41203e2601
Ryan Sonnek <ryan@codecrate.com>
2014-10-21 03:51:46 +0200

    add spring animations

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,9 +1,15 @@
 class HelloController < UIViewController
   extend IB
 
-  attr_accessor :data_source
-
   outlet :btn_hello
+  outlet :label_text
+
+  def viewDidLoad
+    super
+    label_text.slide(:right, 300, duration: 1.0, damping: 0.8, velocity: 2.0) do
+      label_text.slide(:down, 200, duration: 2.0, damping: 0.7, velocity: 1.0)
+    end
+  end
 
   def say_hello(sender)
     alert = UIAlertView.alloc.initWithTitle('Greeting',
